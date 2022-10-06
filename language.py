bd867b89f4eadaf6c5f7e9b0d04142ebd17e3999
zCranking <aravgupta2019@gmail.com>
2022-10-05 17:16:33 -0700

    Tried to fix the error

--- language.py
+++ language.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
-import speech_recognition as sr
-import pyttsx3
 from googletrans import Translator
 
 root = Tk()
@@ -11,10 +9,18 @@ root.geometry("800x600")
 root.config(bg="#EE6352")
 
 title = Label(root, text="Language Transalator", bg="#EE6352", fg="#5465FF", font=("Comic Sans MS", "23", "bold"))
-font = ("Consolas", 22, "normal")
 title.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-text = Text(root, font=("Comic Sans MS", "15", "bold"), height=15, wrap=WORD, width=40, padx=5, pady=5, bd=0)
-text.place(relx=0.5, rely=0.55, anchor=CENTER)
+text = Text(root, font=("Consolas", 22, "normal"), height=15, wrap=WORD, width=40, padx=5, pady=5, bd=0)
+text.place(relx=0.5, rely=0.65, anchor=CENTER)
+
+Thelist = list(LANGUAGES.values())
+languages = ttk.Combobox(state=readonly, values=Thelist[1], width=6)
+languages.place(relx=0.2, rely=0.5, anchor=CENTER)
+languages.set("english")
+
+Output = Label(root, text="Output", bg="#EE6352", font=("Consolas", 22, "normal"))
+Output.place(relx=0.7, rely=0.5, anchor=CENTER)
+
 
 root.mainloop()
